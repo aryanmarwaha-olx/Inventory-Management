e300abc921d1ce7262282c29998dcdd9fc3bab8b
Ayush Garg <ayush.garg@olx.in>
2025-04-13 02:33:38 +0530

    Make Enums

---
+++ src/main/java/in/olx/inventorymanagement/enums/ItemStatus.java
@@ -0,0 +1,7 @@
+package in.olx.inventorymanagement.enums;
+
+public enum ItemStatus {
+    CREATED,
+    PROCURED,
+    SOLD
+}
---
+++ src/main/java/in/olx/inventorymanagement/enums/ItemType.java
@@ -0,0 +1,5 @@
+package in.olx.inventorymanagement.enums;
+
+public enum ItemType {
+    CAR
+}
